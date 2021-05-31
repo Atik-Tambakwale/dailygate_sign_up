9bc4a9fa6a72cf09084e7eb3cb57536886e58298
Atik-Tambakwale <atik20202019@gmail.com>
2021-05-31 16:28:58 +0530

    change localhost url to devops url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/dailygate_sign_up/';
+$config['base_url'] = 'https://devops.dailygate.in/dailygate_sign_up/';
 
 /*
 |--------------------------------------------------------------------------
